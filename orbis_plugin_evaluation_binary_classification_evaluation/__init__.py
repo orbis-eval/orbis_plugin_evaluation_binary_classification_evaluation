1be0d4caffcf5a457d9a9133495de2e206405f68
roger <climberwoodi@gmx.ch>
2021-02-19 08:41:59 +0100

    chg: updated version to 2.3.0

--- orbis_plugin_evaluation_binary_classification_evaluation/__init__.py
+++ orbis_plugin_evaluation_binary_classification_evaluation/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 __name__ = "orbis_plugin_evaluation_binary_classification_evaluation"
-__version__ = "2.2.4"
+__version__ = "2.3.0"
 __author__ = "fabod"
-__year__ = "2019"
+__year__ = "2021"
 __description__ = "The binary_classification_evaluation evaluation plugin for Orbis"
 __license__ = "GPL2"
 __min_python_version__ = "3.6"
